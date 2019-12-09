0b352aa5dc094c936958afbc85c1b69041cbd956
Viko1 <vitaliiropiak@gmail.com>
2019-12-09 22:52:00 +0100

    react-social-page v0.03

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+    <div className='content'>
+              <div>
+                  <img src='https://dbijapkm3o6fj.cloudfront.net/resources/20611,1004,1,6,4,0,960,330/-3842-/20161013141806/image-gallery.jpeg'/>
+              </div>
+              <div className='ava'>
+                  <img src='https://cdn.arstechnica.net/wp-content/uploads/2016/02/5718897981_10faa45ac3_b-640x624.jpg'/>
+                  ava + description
+              </div>
+              <div>
+                  My posts
+              </div>
+              <div>
+                  new post
+              </div>
+              <div>
+                  post 1
+              </div>
+              <div>
+                  post 2
+              </div>
+          </div> )
+}
+
+export default Profile;

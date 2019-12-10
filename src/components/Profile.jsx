434c2c8a7a46b5c8ab5dce1dc0ae232c8909fded
Viko1 <vitaliiropiak@gmail.com>
2019-12-10 23:35:33 +0100

    react-social-page v0.05 module.css

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 const Profile = () => {
     return (
-    <div className='content'>
+    <div className={s.content}>
               <div>
                   <img src='https://dbijapkm3o6fj.cloudfront.net/resources/20611,1004,1,6,4,0,960,330/-3842-/20161013141806/image-gallery.jpeg'/>
               </div>
-              <div className='ava'>
+              <div className={s.ava}>
                   <img src='https://cdn.arstechnica.net/wp-content/uploads/2016/02/5718897981_10faa45ac3_b-640x624.jpg'/>
                   ava + description
               </div>
-              <div>
+              <div className={s.item}>
                   My posts
               </div>
-              <div>
+              <div className={s.item}>
                   new post
               </div>
-              <div>
+              <div className={s.item}>
                   post 1
               </div>
-              <div>
+              <div className={s.item}>
                   post 2
               </div>
           </div> )

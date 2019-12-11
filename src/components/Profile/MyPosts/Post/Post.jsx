720d7b2bfb2427db1898a22749e3085d0a1b813a
Viko1 <vitaliiropiak@gmail.com>
2019-12-11 23:11:03 +0100

    react-social-page v0.06 better folders

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from './Post.module.css'
+
+const Post = () => {
+    return (
+        <div className={s.item}>
+            <img src='https://cdn.arstechnica.net/wp-content/uploads/2016/02/5718897981_10faa45ac3_b-640x624.jpg'/>
+            My posts
+            <div>
+                <span>
+                    like   dislike
+                </span>
+
+            </div>
+        </div>
+    )
+}
+
+export default Post;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import s from './Profile.module.css'
+import MyPosts from "./MyPosts/MyPosts";
+
+const Profile = () => {
+    return (
+        <div className={s.content}>
+            <div>
+                <img
+                    src='https://dbijapkm3o6fj.cloudfront.net/resources/20611,1004,1,6,4,0,960,330/-3842-/20161013141806/image-gallery.jpeg'/>
+            </div>
+            <div>
+                <MyPosts/>
+            </div>
+
+
+        </div>)
+}
+
+export default Profile;

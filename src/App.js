9887296901bb93297e8cfa6f1db89d8e05c5ab80
Viko1 <vitaliiropiak@gmail.com>
2019-12-15 22:31:59 +0100

    react-social-page v0.08 route, browser-router

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 
 function App() {
-  return (
-      <div className='app-wrapper'>
-          <Header/>
-
-          <Navbar/>
-
-          <Profile/>
-      </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/messages' component={Dialogs}/>
+                    <Route path='/news' component={Dialogs}/>
+                    <Route path='/music' component={Dialogs}/>
+                    <Route path='/settings' component={Dialogs}/>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
-
 
 
 export default App;
